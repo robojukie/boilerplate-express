4e80277ff6e4a7ba7d68a0bbaa7ce29f4af6e145
robojukie <juliepaik@utexas.edu>
2019-05-15 09:59:19 -0400

    serve an html file

--- myApp.js
+++ myApp.js
@@ -12,12 +12,15 @@ var app = express();
 console.log("Hello World");
 
 /** 2) A first working Express Server */
-app.get("/", function(req, res) {
-    res.send("Hello World");
-});
+// app.get("/", function(req, res) {
+//     res.send("Hello World");
+// });
 
 /** 3) Serve an HTML file */
-
+let indexPath = __dirname + "/views/index.html";
+app.get("/", (req, res) => {
+    res.sendFile(indexPath);
+});
 
 /** 4) Serve static assets  */
 
